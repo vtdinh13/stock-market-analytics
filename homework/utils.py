dd7df46036c981753c10e52477252701509565fa
Vancesca Dinh <dinh.vancesca@gmail.com>
2025-07-15 14:23:40 +0200

    added functions required to work on hw3

--- homework/utils.py
+++ homework/utils.py
@@ -5,6 +5,9 @@ import re
 import numpy as np
 import time
 import yfinance as yf
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
 
 
 
@@ -77,3 +80,112 @@ def fetch_ticker_data(ticker_list: list) -> pd.DataFrame:
 
     df_all = pd.concat(frames)
     return df_all
+
+def temporal_split(df, min_date, max_date, train_prop=0.7, val_prop=0.15, test_prop=0.15):
+    """
+    Splits a DataFrame into three buckets based on the temporal order of the 'Date' column.
+
+    Args:
+        df (DataFrame): The DataFrame to split.
+        min_date (str or Timestamp): Minimum date in the DataFrame.
+        max_date (str or Timestamp): Maximum date in the DataFrame.
+        train_prop (float): Proportion of data for training set (default: 0.6).
+        val_prop (float): Proportion of data for validation set (default: 0.2).
+        test_prop (float): Proportion of data for test set (default: 0.2).
+
+    Returns:
+        DataFrame: The input DataFrame with a new column 'split' indicating the split for each row.
+    """
+    # Define the date intervals
+    train_end = min_date + pd.Timedelta(days=(max_date - min_date).days * train_prop)
+    val_end = train_end + pd.Timedelta(days=(max_date - min_date).days * val_prop)
+
+    # Assign split labels based on date ranges
+    split_labels = []
+    for date in df['Date']:
+        if date <= train_end:
+            split_labels.append('train')
+        elif date <= val_end:
+            split_labels.append('validation')
+        else:
+            split_labels.append('test')
+
+    # Add 'split' column to the DataFrame
+    df['split'] = split_labels
+
+    return df
+
+TECHNICAL_INDICATORS = ['adx', 'adxr', 'apo', 'aroon_1','aroon_2', 'aroonosc',
+ 'bop', 'cci', 'cmo','dx', 'macd', 'macdsignal', 'macdhist', 'macd_ext',
+ 'macdsignal_ext', 'macdhist_ext', 'macd_fix', 'macdsignal_fix',
+ 'macdhist_fix', 'mfi', 'minus_di', 'mom', 'plus_di', 'dm', 'ppo',
+ 'roc', 'rocp', 'rocr', 'rocr100', 'rsi', 'slowk', 'slowd', 'fastk',
+ 'fastd', 'fastk_rsi', 'fastd_rsi', 'trix', 'ultosc', 'willr',
+ 'ad', 'adosc', 'obv', 'atr', 'natr', 'ht_dcperiod', 'ht_dcphase',
+ 'ht_phasor_inphase', 'ht_phasor_quadrature', 'ht_sine_sine', 'ht_sine_leadsine',
+ 'ht_trendmod', 'avgprice', 'medprice', 'typprice', 'wclprice']
+
+MACRO = ['gdppot_us_yoy', 'gdppot_us_qoq', 'cpi_core_yoy', 'cpi_core_mom', 'FEDFUNDS',
+ 'DGS1', 'DGS5', 'DGS10']
+
+CUSTOM_NUMERICAL = ['SMA10', 'SMA20', 'growing_moving_average', 'high_minus_low_relative','volatility']
+
+CATEGORICAL = ['Month', 'Weekday', 'Ticker', 'ticker_type']
+
+# Decision Tree doesn't like too large and inf. values
+
+def remove_infinite_values(X):
+    """
+    Remove infinite values from the input array.
+
+    Parameters:
+    - X: Input array (NumPy array or array-like)
+
+    Returns:
+    - Array with infinite values removed
+    """
+    return X[np.isfinite(X).all(axis=1)]
+
+
+def remove_outliers_percentile(X, lower_percentile=1, upper_percentile=99):
+    """
+    Remove outliers from the input array based on percentiles.
+
+    Parameters:
+    - X: Input array (NumPy array or array-like)
+    - lower_percentile: Lower percentile threshold (float, default=1)
+    - upper_percentile: Upper percentile threshold (float, default=99)
+
+    Returns:
+    - Array with outliers removed
+    """
+    lower_bound = np.percentile(X, lower_percentile, axis=0)
+    upper_bound = np.percentile(X, upper_percentile, axis=0)
+    mask = np.logical_and(np.all(X >= lower_bound, axis=1), np.all(X <= upper_bound, axis=1))
+    return X[mask]
+
+def fit_decision_tree(X, y, max_depth=20):
+# Initialize the Decision Tree Classifier
+  clf = DecisionTreeClassifier(max_depth=max_depth, random_state=42)
+
+  # Fit the classifier to the training data
+  clf.fit(X, y)
+  return clf, X.columns
+
+def predict_decision_tree(clf:DecisionTreeClassifier, df_X:pd.DataFrame, y_true: pd.Series):
+  # Predict the target variable on the test data
+  y_pred = clf.predict(df_X)
+
+  max_depth = clf.tree_.max_depth
+  # Print the maximum depth
+  print("Maximum depth of the decision tree:", max_depth)
+
+  # Calculate the accuracy/precision of the model
+  accuracy = accuracy_score(y_true, y_pred)
+  precision = precision_score(y_true, y_pred)
+  print(f'Accuracy ={accuracy}, precision = {precision}')
+
+  # resulting df
+  result_df = pd.concat([df_X, y_true, pd.Series(y_pred, index=df_X.index, name='pred_')], axis=1)
+
+  return result_df
